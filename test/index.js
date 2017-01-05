94f7bbd2dd6ba88b471f92b608a1d8a82c9871f2
Anthony Graddy <anthony@locationq.com>
2017-01-05 22:04:27 +0000

    Working

--- test/index.js
+++ test/index.js
@@ -1,7 +1,25 @@
 var tap = require('agraddy.test.tap')(__filename);
+var fs = require('fs');
+var path = require('path');
 
 var mod = require('../');
 
-tap.assert.equal(mod.main('result'), 'result', 'Should be equal.');
+var actual;
+var expected;
 
+process.chdir('test');
+
+// Simple excel file
+actual = mod(getJSON('basic.json').data);
+expected = getJSON('basic.json').expected;
+console.log('actual');
+console.log(actual);
+console.log('expected');
+console.log(expected);
+tap.assert.deepEqual(actual, expected, 'Should be equal.');
+
+
+function getJSON(input) {
+	return require('./fixtures/' + input);
+}
 
